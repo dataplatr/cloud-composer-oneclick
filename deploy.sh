4d50fadb3cdcf613f0b39bdeaa5f0f8aecdb71e3
abhinandansanoor <155422032+abhinandansanoor@users.noreply.github.com>
2024-05-30 23:14:51 +0530

    Add files via upload

---
+++ deploy.sh
@@ -0,0 +1,67 @@
+# Execute the Python script
+python3 src/generate_var_file.py
+
+# Check if the Python script execution was successful
+if [ $? -ne 0 ]; then
+    echo "Failed to generate variable file. Exiting..."
+    exit 1
+fi
+
+# Initialize Terraform (optional)
+terraform init -backend=true
+
+# Check if the Terraform initialization was successful
+if [ $? -ne 0 ]; then
+    echo "Failed to initialize Terraform. Exiting..."
+    exit 1
+fi
+
+# Run Terraform plan with variable file
+terraform plan -var-file=variables.tfvars
+
+# Review the plan and confirm if desired changes
+echo "Please review the Terraform plan above."
+
+# Apply Terraform changes (manual confirmation)
+# terraform apply -var-file=variables.tfvars
+echo -e "\n✅ Cloud Composer environment creation successful."
+
+# Extract configuration values from JSON
+project=$(cat "config/config.json" | python3 -c "import json,sys; print(str(json.load(sys.stdin)['projectId']))" 2>/dev/null || echo "")
+location=$(cat "config/config.json" | python3 -c "import json,sys; print(str(json.load(sys.stdin)['location']))" 2>/dev/null || echo "")
+cloud_composer_env=$(cat "config/config.json" | python3 -c "import json,sys; print(str(json.load(sys.stdin)['composerEnvName']))" 2>/dev/null || echo "")
+
+# Get the Composer environment details
+env_details=$(gcloud composer environments describe $cloud_composer_env \
+  --location $location \
+  --project $project \
+  --format json)
+
+# Check if the gcloud command was successful
+if [ $? -ne 0 ]; then
+    echo "Failed to describe Composer environment. Exiting..."
+    exit 1
+fi
+
+# Parse the environment details to get the bucket name
+bucket_name=$(echo $env_details | jq -r '.config.dagGcsPrefix' | sed 's/^gs:\/\///' | sed 's/\/dags$//')
+
+# Check if the bucket name was successfully parsed
+if [ -z "$bucket_name" ]; then
+    echo "Failed to parse the bucket name from the Composer environment details. Exiting..."
+    exit 1
+fi
+
+# Output the bucket name
+echo "Composer Bucket Name: $bucket_name"
+
+# Copy Oracle DAG files and JSON schema to respective buckets
+gsutil -m cp -r src/Dags/* gs://$bucket_name/dags
+
+# Check if the gsutil command was successful
+if [ $? -ne 0 ]; then
+    echo "Failed to copy DAG files to the bucket. Exiting..."
+    exit 1
+fi
+
+echo "DAG files successfully copied to gs://$bucket_name/dags."
