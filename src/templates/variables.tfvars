a74072fa82f2635290b0ceec74b14d9fbfd1b023
abhinandansanoor <155422032+abhinandansanoor@users.noreply.github.com>
2024-06-02 16:19:12 +0530

    Add files via upload

---
+++ src/templates/variables.tfvars
@@ -0,0 +1,7 @@
+# variables.tfvars
+region = "${location}"
+project_id = "${project_id}"
+project_number = "${projectNumber}"
+composer_env_name = "${composerEnvName}"
+sa_name = "${serviceAccountName}"
+composer_size = "${composer_size}"
---
+++ src/utils/interactive/constants.py
@@ -0,0 +1,11 @@
+"""Data Foundation Deployment UI constants"""
+
+DF_TITLE = ("Create a New <pp fg='#4285F4'>Cloud Composer</pp> <pp fg='#EA4335'>Environment</pp>")
+
+CONFIG_MESSAGE = (
+    "Project : <b>%s</b>.\n"
+    "Location: <b>%s</b>.\n"
+    "Cloud Composer:<b>%s</b>.\n"
+    "Service Account:<b>%s</b>.\n"
+    "Composer Environment Size:<b>%s</b>.\n"
+)
